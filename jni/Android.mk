848eaba7776fec3c4eace3ae32298e250c8574a0
ari222 <sudiptomukh@gmail.com>
2012-04-12 21:24:15 +0530

    Update jni/Android.mk

--- jni/Android.mk
+++ jni/Android.mk
@@ -8,7 +8,7 @@ LOCAL_MODULE := gosmore
 
 LOCAL_CFLAGS := -DANDROID_NDK -DRES_DIR=\"/m8xp2az\"
 
-LOCAL_SRC_FILES := openglespolygon.cpp gosmore.cpp libgosm.cpp 
+LOCAL_SRC_FILES := openglespolygon.cpp NMMapNavigator.cpp libnm.cpp 
 
 LOCAL_LDLIBS := -lGLESv1_CM -ldl -llog
 
